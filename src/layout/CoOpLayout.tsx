bfb1a2f6fefed3bd94e737d4eb0e3a1ec6bde1ca
kaung <kaungmyathun7@gmail.com>
2023-05-30 14:43:30 +0630

    adding: route guard to coop layout

--- src/layout/CoOpLayout.tsx
+++ src/layout/CoOpLayout.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import NavBar from '../components/NavBar/NavBar'
-import Footer from '../components/Footer/Footer'
+import React from "react";
+import NavBar from "../components/NavBar/NavBar";
+import Footer from "../components/Footer/Footer";
+import { useNavigate } from "react-router-dom";
 
 function CoOpLayout() {
-  return (<>
-    <NavBar></NavBar>
-      <div>AdminLayout</div>
-    <Footer></Footer>
+  const navigate = useNavigate();
+  useEffect(() => {
+    const role = localStorage.getItem(btoa("role"));
+    console.log("reload");
+    if (atob(role) !== "coop") {
+      navigate("/", { replace: true });
+    }
+  }, []);
+  return (
+    <>
+      <NavBar></NavBar>
+      <div>COOP Layout</div>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default CoOpLayout
+export default CoOpLayout;
